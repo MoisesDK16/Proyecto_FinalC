9ec9023f5a9c13e9a72b1e9047cd4e44bfb8510c
moisesDK16 <moisesloor12@gmail.com>
2024-07-09 00:22:29 -0500

    aas

--- App_Cursos/config/conexion.php
+++ App_Cursos/config/conexion.php
@@ -1,1 +1,23 @@
-//hola
+<?php
+
+class Clase_Conectar {
+    private $conexion;
+    private $server = "localhost";
+    private $usuario = "root"; 
+    private $pass = ""; 
+    private $base = "cursos";
+
+    public function conectar() {
+        $this->conexion = new mysqli($this->server, $this->usuario, $this->pass, $this->base);
+        
+        if ($this->conexion->connect_error) {
+            die("Error al conectar con MySQL: " . $this->conexion->connect_error);
+        }
+
+        if (!$this->conexion->set_charset("utf8")) {
+            die("Error al establecer el charset UTF-8: " . $this->conexion->error);
+        }
+        return $this->conexion;
+    }   
+}
+?>
