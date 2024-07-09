bede38642f898ced67f4668b8d5b6c07737588af
moisesDK16 <moisesloor12@gmail.com>
2024-07-09 01:22:49 -0500

    ghghhg

--- App_Cursos/controllers/aula.controller.php
+++ App_Cursos/controllers/aula.controller.php
@@ -1,1 +1,32 @@
+ <?php 
+ require_once("../models/aula.model.php");
 
+$aula = new Clase_Aula(); 
+
+switch($_GET["op"]){
+
+    case "insertar":
+        $datos = json_decode(file_get_contents("php://input"));
+
+        if(!empty($datos->numero_aula && $datos->capacidad)){
+            $aula->registrarAula($datos->numero_aula, $datos->capacidad);
+            echo "Aula registrada";
+        }else{
+            echo "Error al registrar aula";
+        }
+        
+    break;
+
+    case "listar": 
+        $aulas= array();
+        $dato = $aula->listarAulas();
+        if ($fila = mysqli_fetch_assoc($dato)) {
+            $aulas[] = $fila;
+            echo json_encode($aulas);
+        } else {
+            $aulas[] = [];
+        }
+
+    break;
+
+}
